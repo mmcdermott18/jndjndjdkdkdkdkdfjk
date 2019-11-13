b94f2e5b46bbc475dfd9cbfc4025d6a9651f35ab
Michael M <mmcdermott18@users.noreply.github.com>
2019-11-12 20:53:21 -0500

    updates

--- e17/js/main.js
+++ e17/js/main.js
@@ -1,21 +1,8 @@
-// everything must go in the document ready function
-$( document ).ready(function() {
+$(document).ready(function(){
 
-  // prints to the console which we can see through the inspector
-  console.log( 'ready!' );
+  console.log('THE DOCUMENT IS READY! DO SOMETHING AWESOME!');
 
-  // allows us to make an interaction on click
-  $('p').click(function() {
-
-    // grows the font size coded through jquery
-    // $('p').css('font-size','10vw');
-
-    // toggles a class we made in css. one click adds it, the next click removes it
-    $('p').toggleClass('grow');
-
+  $('.snow').click(function() {
+    $('.number-1').toggleClass('align-left');
   });
-
-  // grows the font size when jquery loads
-  // $('p').css('font-size','10vw');
-  
 });
